ab578e62fd7498da8071613033591e69f94c585d
Jonathan Hefner <jonathan@hefner.pro>
2018-10-14 12:20:50 -0500

    Add ModelSearch::criteria

--- lib/talent_scout/model_search.rb
+++ lib/talent_scout/model_search.rb
@@ -1,5 +1,13 @@
 module TalentScout
   class ModelSearch
     include ActiveModel::Model
+    include ActiveModel::Attributes
+
+    def self.criteria(names, type = :string)
+      Array(names).each do |name|
+        attribute name, type
+      end
+    end
+
   end
 end
--- test/model_search_test.rb
+++ test/model_search_test.rb
@@ -7,9 +7,24 @@ class ModelSearchTest < Minitest::Test
     assert_kind_of ActiveModel::Model, MyModelSearch.new
   end
 
+  def test_attribute_assignment_type_casts_values
+    expected = {
+      str1: "abc",
+      int1: 123,
+      date1: Date.new(1999, 12, 31),
+      date2: Date.new(2000, 01, 01),
+    }
+
+    search = MyModelSearch.new(expected.transform_values(&:to_s))
+    assert_equal expected, search.attributes.symbolize_keys
+  end
+
   private
 
   class MyModelSearch < TalentScout::ModelSearch
+    criteria :str1
+    criteria :int1, :integer
+    criteria %i[date1 date2], :date
   end
 
 end
