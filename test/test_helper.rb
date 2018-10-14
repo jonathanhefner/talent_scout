a28ce1914a97aa0c34d7ba3a4abca27c48b5fc9a
Jonathan Hefner <jonathan@hefner.pro>
2018-10-14 11:31:25 -0500

    Use fixtures from dummy app instead of engine

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,11 +11,3 @@ Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
 require "rails/test_unit/reporter"
 Rails::TestUnitReporter.executable = "rake test"
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-  ActiveSupport::TestCase.fixtures :all
-end
