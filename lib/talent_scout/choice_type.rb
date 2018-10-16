94828ed4b1ddb15dea52d65f8a55a551608c16f1
Jonathan Hefner <jonathan@hefner.pro>
2018-10-16 11:44:50 -0500

    Allow cast of ChoiceType underlying values

--- lib/talent_scout/choice_type.rb
+++ lib/talent_scout/choice_type.rb
@@ -4,11 +4,17 @@ module TalentScout
     attr_reader :choices
 
     def initialize(choices)
-      @choices = choices.stringify_keys
+      @choices = choices.stringify_keys.merge!(choices) do |key, old_val, new_val|
+        next old_val if old_val == new_val
+        raise ArgumentError.new("Multiple possible values for key #{key.inspect}")
+      end.merge!(choices.values.index_by(&:itself)) do |key, old_val, new_val|
+        next old_val if old_val == new_val
+        raise ArgumentError.new("Value #{key.inspect} is also a key")
+      end
     end
 
     def cast(value)
-      super(@choices[value.to_s])
+      super(@choices[value])
     end
 
   end
--- test/choice_type_test.rb
+++ test/choice_type_test.rb
@@ -3,23 +3,50 @@ require "talent_scout"
 
 class ChoiceTypeTest < Minitest::Test
 
-  def test_cast
+  def test_cast_with_valid_value
     type = TalentScout::ChoiceType.new(CHOICES)
-
     CHOICES.each do |value, expected|
+      assert_equal expected, type.cast(expected)
       assert_equal expected, type.cast(value)
       assert_equal expected, type.cast(value.to_s)
     end
+  end
 
+  def test_cast_with_invalid_value
+    type = TalentScout::ChoiceType.new(CHOICES)
     assert_nil type.cast("BAD")
   end
 
+  def test_choices_with_aliasing_keys
+    type = TalentScout::ChoiceType.new({ "foo" => true, foo: true })
+    assert_equal true, type.cast("foo")
+    assert_equal true, type.cast(:foo)
+  end
+
+  def test_choices_with_conflicting_values
+    assert_raises(ArgumentError) do
+      TalentScout::ChoiceType.new({ "foo" => true, foo: false })
+    end
+  end
+
+  def test_choices_with_self_referential_key
+    type = TalentScout::ChoiceType.new({ "foo" => true, true => true })
+    assert_equal true, type.cast("foo")
+    assert_equal true, type.cast(true)
+  end
+
+  def test_choices_with_ambiguous_key_value
+    assert_raises(ArgumentError) do
+      TalentScout::ChoiceType.new({ "foo" => true, true => 2 })
+    end
+  end
+
   def test_attribute_assignment
-    params = { foo: CHOICES.first.first.to_s }
-    expected = CHOICES.first.last
+    params = { choice1: CHOICES.first[0].to_s }
+    expected = CHOICES.first[1]
 
     model = MyModel.new(params)
-    assert_equal expected, model.foo
+    assert_equal expected, model.choice1
   end
 
   private
@@ -30,7 +57,7 @@ class ChoiceTypeTest < Minitest::Test
     include ActiveModel::Model
     include ActiveModel::Attributes
 
-    attribute :foo, TalentScout::ChoiceType.new(CHOICES)
+    attribute :choice1, TalentScout::ChoiceType.new(CHOICES)
   end
 
 end
