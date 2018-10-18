c4f9d6e3bef038eecf058a0e4912ae3249363cae
Jonathan Hefner <jonathan@hefner.pro>
2018-10-17 19:02:56 -0500

    Implement ModelSearch#results with criteria blocks

--- lib/talent_scout/model_search.rb
+++ lib/talent_scout/model_search.rb
@@ -7,12 +7,47 @@ module TalentScout
       @model ||= self.name.chomp("Search").constantize
     end
 
-    def self.criteria(names, type = :string)
-      type = ChoiceType.new(type) if type.is_a?(Hash)
+    def self.criteria(names, type = :string, &block)
+      crit = Criteria.new(names, type, &block)
 
-      Array(names).each do |name|
-        attribute name, OrMissingType.new(type), default: OrMissingType::MISSING
+      crit.names.each do |name|
+        attribute name, crit.type, default: crit.default
       end
+
+      self.criteria_list << crit
+    end
+
+    def results
+      attributes = self.attributes
+      self.class.criteria_list.reduce(self.class.model.all) do |scope, crit|
+        crit.apply(scope, attributes)
+      end
+    end
+
+    private
+
+    class Criteria
+      attr_reader :names, :type, :default, :block
+
+      def initialize(names, type, &block)
+        @names = Array(names).map(&:to_s)
+        @type = OrMissingType.new(type.is_a?(Hash) ? ChoiceType.new(type) : type)
+        @default = OrMissingType::MISSING
+        @block = block
+      end
+
+      def apply(scope, attributes)
+        block_args = names.map{|name| attributes[name] }
+        if block_args.none?{|value| OrMissingType::MISSING == value }
+          scope.instance_exec(*block_args, &block)
+        else
+          scope
+        end
+      end
+    end
+
+    def self.criteria_list
+      @criteria_list ||= []
     end
 
   end
--- test/model_search_test.rb
+++ test/model_search_test.rb
@@ -8,16 +8,8 @@ class ModelSearchTest < Minitest::Test
   end
 
   def test_attribute_assignment_type_casts_values
-    expected = {
-      str1: "abc",
-      int1: 123,
-      date1: Date.new(1999, 12, 31),
-      date2: Date.new(2000, 01, 01),
-      choice1: :foo,
-    }
-
-    search = MyModelSearch.new(expected.transform_values(&:to_s))
-    assert_equal expected, search.attributes.symbolize_keys
+    search = MyModelSearch.new(CRITERIA_VALUES.transform_values(&:to_s))
+    assert_equal CRITERIA_VALUES, search.attributes.symbolize_keys
   end
 
   def test_attribute_default_values
@@ -31,16 +23,70 @@ class ModelSearchTest < Minitest::Test
     assert_equal MyModel, MyModelSearch.model
   end
 
+  def test_results_with_full_criteria_values
+    search = MyModelSearch.new(CRITERIA_VALUES)
+    assert_results CRITERIA_VALUES, search
+  end
+
+  def test_results_with_missing_criteria_values
+    group_regexp = /^(.+?)(?:_part\d)?$/
+    groups = CRITERIA_VALUES.keys.group_by{|name| name.to_s[group_regexp, 1] }.values
+    groups.each do |group|
+      group.each do |name|
+        search = MyModelSearch.new(CRITERIA_VALUES.except(name))
+        assert_results CRITERIA_VALUES.except(*group), search
+      end
+    end
+  end
+
   private
 
   class MyModel
+    def self.all
+      MockRelation.new
+    end
+
+    class MockRelation
+      attr_reader :to_a
+
+      def initialize(to_a = [])
+        @to_a = to_a
+      end
+
+      def append(name, value)
+        self.class.new(@to_a + [[name, value, :OK]])
+      end
+    end
   end
 
   class MyModelSearch < TalentScout::ModelSearch
-    criteria :str1
-    criteria :int1, :integer
-    criteria %i[date1 date2], :date
-    criteria :choice1, { "foo" => :foo, "bar" => :bar }
+    criteria :str1 do |x|
+      append(:str1, x)
+    end
+
+    criteria :int1, :integer do |x|
+      append(:int1, x)
+    end
+
+    criteria %i[date1_part1 date1_part2], :date do |x, y|
+      append(:date1_part1, x).append(:date1_part2, y)
+    end
+
+    criteria :choice1, { "foo" => :foo, "bar" => :bar } do |x|
+      append(:choice1, x)
+    end
+  end
+
+  CRITERIA_VALUES = {
+    str1: "abc",
+    int1: 123,
+    date1_part1: Date.new(1999, 12, 31),
+    date1_part2: Date.new(2000, 01, 01),
+    choice1: :foo,
+  }
+
+  def assert_results(criteria_values, search)
+    assert_equal criteria_values.map{|k, v| [k, v, :OK] }, search.results.to_a
   end
 
 end
