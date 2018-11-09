8bc527b3d4c62cd9c7d8cdc4dda655e73a6d0905
Jonathan Hefner <jonathan@hefner.pro>
2018-11-09 11:04:29 -0600

    Factor Criteria out to separate file

--- lib/talent_scout.rb
+++ lib/talent_scout.rb
@@ -1,6 +1,7 @@
 require "talent_scout/version"
 require "talent_scout/choice_type"
 require "talent_scout/void_type"
+require "talent_scout/criteria"
 require "talent_scout/model_search"
 require "talent_scout/controller"
 require "talent_scout/railtie"
--- lib/talent_scout/model_search.rb
+++ lib/talent_scout/model_search.rb
@@ -69,56 +69,6 @@ module TalentScout
 
     private
 
-    class Criteria
-      attr_reader :names, :type, :block
-
-      def initialize(names, type, &block)
-        @names = Array(names).map(&:to_s)
-        @type = case type
-          when ActiveModel::Type::Value
-            type
-          when :void
-            VoidType.new
-          when Hash, Array
-            ChoiceType.new(type)
-          else
-            ActiveRecord::Type.lookup(type)
-          end
-        @block = block
-      end
-
-      def apply(scope, attribute_set)
-        if applicable?(attribute_set)
-          if block
-            block_args = names.map{|name| attribute_set[name].value }
-            if block.arity == -1 # block from Symbol#to_proc
-              scope.instance_exec(scope, *block_args, &block)
-            else
-              scope.instance_exec(*block_args, &block)
-            end || scope
-          else
-            where_args = names.reduce({}){|h, name| h[name] = attribute_set[name].value; h }
-            scope.where(where_args)
-          end
-        else
-          scope
-        end
-      end
-
-      def applicable?(attribute_set)
-        names.all? do |name|
-          attribute = attribute_set[name]
-          if attribute.came_from_user?
-            if type.is_a?(TalentScout::VoidType)
-              !attribute.value.nil?
-            else
-              !attribute.value.nil? || attribute.value_before_type_cast.nil?
-            end
-          end
-        end
-      end
-    end
-
     def self.criteria_list
       @criteria_list ||= self == ModelSearch ? [] : self.superclass.criteria_list.dup
     end
