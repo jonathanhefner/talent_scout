3268d3b0eff2672e2adf80901ffcacd616dfdc86
Jonathan Hefner <jonathan@hefner.pro>
2019-01-06 13:08:53 -0600

    Refactor OrderDefinitionTest

--- test/order_definition_test.rb
+++ test/order_definition_test.rb
@@ -9,54 +9,54 @@ class OrderDefinitionTest < Minitest::Test
   end
 
   def test_asc_choice
-    definition = TalentScout::OrderDefinition.new("foo", ALL_COLUMNS, asc_suffix: "bar")
-    assert_equal "foobar", definition.asc_choice
+    definition = make_definition(asc_suffix: "bar")
+    assert_equal (definition.name + "bar"), definition.asc_choice
   end
 
   def test_desc_choice
-    definition = TalentScout::OrderDefinition.new("foo", ALL_COLUMNS, desc_suffix: "bar")
-    assert_equal "foobar", definition.desc_choice
+    definition = make_definition(desc_suffix: "bar")
+    assert_equal (definition.name + "bar"), definition.desc_choice
   end
 
   def test_desc_choice_with_only_static_columns
-    definition = TalentScout::OrderDefinition.new("foo", STATIC_COLUMNS)
+    definition = make_definition(STATIC_COLUMNS)
     assert_equal definition.asc_choice, definition.desc_choice
   end
 
   def test_default_suffixes
-    definition = TalentScout::OrderDefinition.new("foo", ALL_COLUMNS)
+    definition = make_definition()
     refute_equal definition.asc_choice, definition.desc_choice
   end
 
   def test_asc_value
-    definition = TalentScout::OrderDefinition.new("foo", ALL_COLUMNS)
+    definition = make_definition()
     expected = ALL_COLUMNS.join(", ")
     assert_equal Arel.sql(expected), definition.asc_value
   end
 
   def test_desc_value
-    definition = TalentScout::OrderDefinition.new("foo", ALL_COLUMNS)
+    definition = make_definition()
     expected = (DYNAMIC_COLUMNS.map{|c| "#{c} DESC" } + STATIC_COLUMNS).join(", ")
     assert_equal Arel.sql(expected), definition.desc_value
   end
 
   def test_symbol_columns
-    actual = TalentScout::OrderDefinition.new("foo", ALL_COLUMNS.map(&:to_sym))
-    expected = TalentScout::OrderDefinition.new("foo", ALL_COLUMNS)
+    expected = make_definition(ALL_COLUMNS.map(&:to_s))
+    actual = make_definition(ALL_COLUMNS.map(&:to_sym))
     assert_equal expected.asc_value, actual.asc_value
     assert_equal expected.desc_value, actual.desc_value
   end
 
   def test_singular_column
-    actual = TalentScout::OrderDefinition.new("foo", ALL_COLUMNS.first)
-    expected = TalentScout::OrderDefinition.new("foo", ALL_COLUMNS.take(1))
+    expected = make_definition(ALL_COLUMNS.take(1))
+    actual = make_definition(ALL_COLUMNS.first)
     assert_equal expected.asc_value, actual.asc_value
     assert_equal expected.desc_value, actual.desc_value
   end
 
   def test_nil_columns
-    actual = TalentScout::OrderDefinition.new("foo", nil)
-    expected = TalentScout::OrderDefinition.new("foo", ["foo"])
+    expected = make_definition([make_definition.name])
+    actual = make_definition(nil)
     assert_equal expected.asc_value, actual.asc_value
     assert_equal expected.desc_value, actual.desc_value
   end
@@ -66,5 +66,9 @@ class OrderDefinitionTest < Minitest::Test
   DYNAMIC_COLUMNS = ["col1", "col2", "col3"]
   STATIC_COLUMNS = ["col4 ASC", "col5 asc", "col6 DESC", "col7 desc"]
   ALL_COLUMNS = DYNAMIC_COLUMNS + STATIC_COLUMNS
+
+  def make_definition(columns = ALL_COLUMNS, **options)
+    TalentScout::OrderDefinition.new("my_order", columns, options)
+  end
 
 end
