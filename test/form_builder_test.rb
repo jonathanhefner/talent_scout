3ce92be97ccc68522a465611c7122f7da3c03fb1
Jonathan Hefner <jonathan@hefner.pro>
2019-01-10 11:47:14 -0600

    Refactor FormBuilderTest

--- test/form_builder_test.rb
+++ test/form_builder_test.rb
@@ -8,40 +8,33 @@ class FormBuilderTest < ActionView::TestCase
   end
 
   def test_form_uses_specified_params_wrapper
-    field_name = :date1
-    expected = text_field TalentScout::PARAM_KEY, field_name
-    actual = make_form(MyModelSearch.new).text_field(field_name)
-    assert_equal expected, actual
+    field = make_form.text_field(:date1)
+    assert_html_attribute "name", "#{TalentScout::PARAM_KEY}[date1]", field
   end
 
   def test_form_uses_value_before_type_cast
-    before_type_cast = "December 31, 1999"
-    search = MyModelSearch.new(date1: before_type_cast)
-    refute_equal before_type_cast, search.date1.to_s # sanity check
-    field = make_form(search).text_field(:date1)
-    assert_includes field, before_type_cast
+    refute_equal SEARCH.date1_before_type_cast, SEARCH.date1.to_s # sanity check
+    field = make_form.text_field(:date1)
+    assert_html_attribute "value", SEARCH.date1_before_type_cast, field
   end
 
   def test_select_accepts_each_choice_raw_enum
-    search = MyModelSearch.new
-    each_choice = search.each_choice(:choice1)
+    each_choice = SEARCH.each_choice(:choice1)
     choices = each_choice.to_a.map(&:first)
-    form = make_form(search)
+    form = make_form
     assert_equal options_for_select(choices), options_for_select(each_choice)
     assert_equal form.select(:choice1, choices), form.select(:choice1, each_choice)
   end
 
   def test_select_selects_value_before_type_cast
-    before_type_cast = "two"
-    search = MyModelSearch.new(choice1: before_type_cast)
-    field = make_form(search).select(:choice1, search.each_choice(:choice1))
+    before_type_cast = SEARCH.choice1_before_type_cast
     selected = options_for_select([before_type_cast], selected: before_type_cast)
+    field = make_form.select(:choice1, SEARCH.each_choice(:choice1))
     assert_includes field, selected
   end
 
   def test_submit_button_default_text
-    form = make_form(MyModelSearch.new)
-    assert_equal form.submit("Search"), form.submit
+    assert_html_attribute "value", "Search", make_form.submit
   end
 
   private
@@ -59,8 +52,14 @@ class FormBuilderTest < ActionView::TestCase
     "/#{MyModel.model_name.route_key}"
   end
 
-  def make_form(search)
+  SEARCH = MyModelSearch.new(date1: "December 31, 1999", choice1: "two")
+
+  def make_form(search = SEARCH)
     form_with(model: search){|form| return form }
+  end
+
+  def assert_html_attribute(attribute_name, expected_value, html)
+    assert_includes html, " #{attribute_name}=\"#{expected_value}\""
   end
 
 end
