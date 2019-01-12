ce01a86413a572b817c9dec6e68382dcd02b537c
Jonathan Hefner <jonathan@hefner.pro>
2019-01-12 14:27:57 -0600

    Abstract FormBuilderTest

--- test/form_builder_test.rb
+++ test/form_builder_test.rb
@@ -4,37 +4,38 @@ require "talent_scout"
 class FormBuilderTest < ActionView::TestCase
 
   def test_form_action_points_to_model_controller
-    assert_equal form_with(model: MyModel.new), form_with(model: MyModelSearch.new)
+    expected = form_builder_test_my_models_path
+    assert_html_attribute "action", expected, build_form(MyModel.new) # sanity check
+    assert_html_attribute "action", expected, build_form(MyModelSearch.new)
   end
 
   def test_form_uses_specified_params_wrapper
-    field = make_form.text_field(:date1)
+    field = make_text_field(:date1)
     assert_html_attribute "name", "#{TalentScout::PARAM_KEY}[date1]", field
   end
 
   def test_form_uses_value_before_type_cast
     refute_equal SEARCH.date1_before_type_cast, SEARCH.date1.to_s # sanity check
-    field = make_form.text_field(:date1)
+    field = make_text_field(:date1)
     assert_html_attribute "value", SEARCH.date1_before_type_cast, field
   end
 
   def test_select_accepts_each_choice_raw_enum
     each_choice = SEARCH.each_choice(:choice1)
     choices = each_choice.to_a.map(&:first)
-    form = make_form
     assert_equal options_for_select(choices), options_for_select(each_choice)
-    assert_equal form.select(:choice1, choices), form.select(:choice1, each_choice)
+    assert_equal make_select_field(:choice1, choices), make_select_field(:choice1, each_choice)
   end
 
   def test_select_selects_value_before_type_cast
     before_type_cast = SEARCH.choice1_before_type_cast
     selected = options_for_select([before_type_cast], selected: before_type_cast)
-    field = make_form.select(:choice1, SEARCH.each_choice(:choice1))
+    field = make_select_field(:choice1, SEARCH.each_choice(:choice1))
     assert_includes field, selected
   end
 
   def test_submit_button_default_text
-    assert_html_attribute "value", "Search", make_form.submit
+    assert_html_attribute "value", "Search", make_submit_button()
   end
 
   private
@@ -54,8 +55,24 @@ class FormBuilderTest < ActionView::TestCase
 
   SEARCH = MyModelSearch.new(date1: "December 31, 1999", choice1: "two")
 
-  def make_form(search = SEARCH)
-    form_with(model: search){|form| return form }
+  def build_form(model, &block)
+    form_with(model: model, &block)
+  end
+
+  def make_form_builder
+    @form_builder ||= build_form(SEARCH){|form| break form }
+  end
+
+  def make_text_field(name)
+    make_form_builder.text_field(name)
+  end
+
+  def make_select_field(name, choices)
+    make_form_builder.select(name, choices)
+  end
+
+  def make_submit_button(value = nil)
+    make_form_builder.submit(value)
   end
 
   def assert_html_attribute(attribute_name, expected_value, html)
