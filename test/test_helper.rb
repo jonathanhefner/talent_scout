e2b2cac34010874b561d527491feb4a21ed66988
Jonathan Hefner <jonathan@hefner.pro>
2020-01-04 12:59:23 -0600

    Tune backtrace silencers in an idiomatic way

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,12 +5,7 @@ require_relative "../test/dummy/config/environment"
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 require "rails/test_help"
 
-Rails.backtrace_cleaner.remove_silencers!
-Rails.backtrace_cleaner.add_silencer{|line| !line.include?(File.basename(File.dirname(__dir__))) }
-
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
-Minitest.backtrace_filter = Minitest::BacktraceFilter.new
+Rails.backtrace_cleaner.add_filter{|line| line.sub("#{File.dirname(__dir__)}/", "") }
 
 require "rails/test_unit/reporter"
 Rails::TestUnitReporter.executable = "rake test"
