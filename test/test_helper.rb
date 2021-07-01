87aebf62943ac590ae2761717b870de32a2fe70c
Jonathan Hefner <jonathan@hefner.pro>
2021-07-01 10:55:45 -0500

    Remove backtrace cleaner configuration
    Since rails/rails@33fdae0584b5715d7111d91360792e62f188db92, the default
    backtrace cleaner configuration no longer causes empty backtraces.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,4 @@ require_relative "../test/dummy/config/environment"
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 require "rails/test_help"
 
-Rails.backtrace_cleaner.add_filter{|line| line.sub("#{File.dirname(__dir__)}/", "") }
-
 ActiveSupport::TestCase.fixtures :all
